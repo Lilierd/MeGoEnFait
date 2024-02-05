349c8e98e7591a447779119aa69bbab5f1952e81
Lilierd 42 <lilierdyt@gmail.com>
2024-02-05 15:51:37 +0100

    Update trajethandler.go

--- project/bastien/trajethandler.go
+++ project/bastien/trajethandler.go
@@ -1,5 +1,46 @@
 package bastien
 
-func Dijsktra() {
+import (
+	"fmt"
+	"project/bastien/trajet"
+	"project/bastien/ville"
+)
 
+func Dijsktra(nomVille1 string, nomVille2 string) {
+	//depart := ville.New()
+	
+}
+
+func GetNbVilles(tab []trajet.Trajet) int {
+	i := 0
+	already := GetVilles(tab)
+
+	for _, v := range already {
+		v.IsVisited()
+		i++
+	}
+
+	return i
+}
+
+func GetVilles(tab []trajet.Trajet) []ville.Ville {
+	var already []ville.Ville
+
+	for _, traj := range tab {
+		var vi ville.Ville
+		src := true
+		for _, v := range already {
+			if traj.GetVille1().Nom == v.Nom || traj.GetVille2().Nom == v.Nom {
+				vi = v
+				fmt.Println(vi.Nom)
+				src = false
+				break;
+			}		
+		}
+		if src == true {
+			already = append(already, vi)
+		}
+	}
+
+	return already
 }
